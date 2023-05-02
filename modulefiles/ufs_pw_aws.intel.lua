96b0136e7ac1ee9ad1a4cdd4e3fd21950b347a84
Sadegh Sadeghi Tabas <31417680+SadeghTabas-NOAA@users.noreply.github.com>
2023-05-02 04:07:19 -0400

    Create ufs_pw_aws.intel.lua

---
+++ modulefiles/ufs_pw_aws.intel.lua
@@ -0,0 +1,20 @@
+help([[
+loads UFS Model prerequisites for pw-aws/intel
+]])
+
+
+prepend_path("MODULEPATH", "/contrib/spack-stack/envs/ufswm/install/modulefiles/Core")
+
+load("stack-intel")
+load("stack-intel-oneapi-mpi")
+load("stack-python")
+load("cmake/3.22.1")
+
+load("ufs_common_spack")
+
+setenv("CC", "mpiicc")
+setenv("CXX", "mpiicpc")
+setenv("FC", "mpiifort")
+setenv("CMAKE_Platform", "pw_aws.intel")
+
+whatis("Description: UFS build environment")
